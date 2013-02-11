9ad98232b7745a790af8e7d3596ee973ec933f9b
Ceasar Bautista <cbautista2010@gmail.com>
2013-02-10 23:05:46 -0500

    Basic schema

--- app/models.py
+++ app/models.py
@@ -1,3 +1,58 @@
+from mongoengine import (connect, Document, EmbeddedDocument, StringField,
+                         ReferenceField,
+                         EmailField,
+                         EmbeddedDocumentField,
+                         DateTimeField,
+                         GeoPointField,
+                         URLField,
+                         ListField)
 
-# class Event(db.Model):
-#    pass
+import config
+
+
+# Connect to the database or create it if it does not exist
+connect(config.DB_NAME)
+
+
+class Event(Document):
+    """
+    An event created by a group.
+    """
+    # NOTE: Picture can probably be grabbed from Facebook API
+    # NOTE: Enforce uniqueness here?
+    name = StringField(required=True)
+    description = StringField()
+    datetime = DateTimeField()
+    location = GeoPointField()
+
+    creators = ListField(ReferenceField('User'))
+
+
+class User(Document):
+    """
+    Abstract user.
+    """
+    name = StringField(required=True, unique=True)
+    email = EmailField(required=True, unique=True)
+
+
+class Student(User):
+    """
+    A student.
+    """
+    subscriptions = ListField(EmbeddedDocumentField('Subscription'))
+
+
+class Subscription(EmbeddedDocument):
+    """
+    Subscriptions can be created by filtering Events by User, Tag, and Type.
+    """
+    name = StringField(required=True)
+    tags = ListField(ReferenceField('Tag'))
+
+
+class Tag(Document):
+    """
+    Arbitrary tag for Events.
+    """
+    name = StringField(required=True)
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ basedir = os.getcwd()
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+DB_NAME = "test_db"
