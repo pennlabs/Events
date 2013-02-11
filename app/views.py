6ed6289210e9712840eeb753197d836f1cc70e0b
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-02-10 22:37:24 -0500

    created skeleton controller

--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return 0
+
+
+@app.route('/user/create', methods=['POST'])
+def create_user():
+    return 0
