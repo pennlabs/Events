423574044a5413708d0d854f6bffaff10e82bf27
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-02-11 19:37:51 -0500

    Added pymongo and user login and creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import models, views
+connection = MongoClient()
+db = connection.events
+
+from app import views
