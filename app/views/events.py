d68a6e077c760f3214cd10c01bbb1a8ea2dc0708
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-04-01 20:35:26 -0400

    Removed print and renamed arguments

--- app/views/events.py
+++ app/views/events.py
@@ -15,11 +15,11 @@ class EventAPI(BSONAPI):
 
     def post(self):
         # it might either be in form data or request data
-        entity = request.form.to_dict() or request.json
-        self.collection.insert(entity)
+        event = request.form.to_dict() or request.json
+        self.collection.insert(event)
         # signals that a new event was made
-        new_event_signal.send(self, entity=entity, u_id=session['user'])
-        return jsonify(entity)
+        new_event_signal.send(self, event=event, u_id=session['user'])
+        return jsonify(event)
 
 register_api(EventAPI, 'event_api', 'events')
 
@@ -27,16 +27,13 @@ register_api(EventAPI, 'event_api', 'events')
 @new_event_signal.connect
 def new_event_triggered(sender=None, **kwargs):
     """
-    On the signalling of a new event, add the event to its creator's
-    event list.
+    When an event is created, add the event to its creator's event list.
     """
-    print "event triggerd"
     u_id = ObjectId(kwargs['u_id'])
-    e_id = ObjectId(kwargs['entity']['_id'])
+    e_id = ObjectId(kwargs['event']['_id'])
     # insert the event into the creator's event list
     g.db.users.update({'_id': u_id}, {'$push': {'events': e_id}}, upsert=True)
     # insert e_id into followers' event queues
-    # TODO: write tests to ensure that all followers' event queues are updated
     g.db.users.update({'following': u_id},
                       {'$push': {'event_queue': e_id}},
                       upsert=True, multi=True)
