e59cff216ecbbb9c310bd7973b705281f322427b
Ceasar Bautista <cbautista2010@gmail.com>
2013-04-20 11:04:42 -0400

    Add g.current_user

--- app/views/general.py
+++ app/views/general.py
@@ -4,7 +4,6 @@ from flask import render_template, g
 from bson.objectid import ObjectId
 
 from app import app
-from app.lib.auth import get_current_user
 from app.lib.json import jsonify
 
 
@@ -13,27 +12,20 @@ from app.lib.json import jsonify
 @app.route('/create', methods=['GET'])
 @app.route('/')
 def index():
-    user = get_current_user()
-    if user:
-        user['logged_in'] = True
-    return render_template('index.html', user=jsonify(user))
+    return render_template('index.html', user=jsonify(g.current_user))
 
 
 @app.route('/event/<event_id>', methods=['GET'])
 def event(event_id):
-    user = get_current_user()
     event_to_render = g.db.events.find_one({'_id': ObjectId(event_id)})
     return render_template('index.html',
-                           user=jsonify(user),
+                           user=jsonify(g.current_user),
                            event_to_render=jsonify(event_to_render))
 
 
 @app.route('/user/<user_id>', methods=['GET'])
 def user(user_id):
-    user = get_current_user()
-    if user:
-        user['logged_in'] = True
     user_to_render = g.db.users.find_one({'_id': ObjectId(user_id)})
     return render_template('index.html',
-                           user=jsonify(user),
+                           user=jsonify(g.current_user),
                            user_to_render=jsonify(user_to_render))
